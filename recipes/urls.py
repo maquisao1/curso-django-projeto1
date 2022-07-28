96ce95ac1ac3d22f2596d68a83068e000bfd2e6c
Max <maquisao1@gmail.com>
2022-07-28 09:57:42 -0300

    css nao vai no `saiba mais`

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path
 
-from recipes.views import home
+from . import views
 
-# dominio/recipes/
 urlpatterns = [
 
-    path('', home),
-
+    path('', views.home),
+    path('recipes/<int:id>/', views.recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -2,6 +2,12 @@ from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={
+    return render(request, 'recipes/pages/home.html', context={
+        'name': 'Max Pinheiro',
+    })
+
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/recipe-view.html', context={
         'name': 'Max Pinheiro',
     })
